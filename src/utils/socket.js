423eb2cc511eb0cbb2a3f5f905a05cac04adbc59
Alisadaintanvir <alisadaintanvir@gmail.com>
2025-03-19 20:52:39 +0600

    refactor socket connection to use API endpoint from configuration

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
+import API_CONFIG from "./apiConstants";
 
-const socket = io("http://localhost:8000");
+const socket = io(API_CONFIG.API_ENDPOINT);
 export default socket;
